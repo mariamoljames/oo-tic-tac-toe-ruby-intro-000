3ee063091d4b82be5521fffbdb75dd8c2b580acd
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-11 16:52:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,3 +24,11 @@ class TicTacToe
   def position_taken?(index)
     @board[index]="X"||@board[index]="O"
   end
+
+  def valid_move?(board,index)
+    if index.between?(0,8) && !(position_taken?(board,index))
+      true
+    else
+      false
+    end
+  end
